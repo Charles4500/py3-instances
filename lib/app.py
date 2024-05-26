0de03ce5d52e9bde24cd7a0d572c05df2de4df1c
CharlesKibet <charlesbiegon973@gmail.com>
2024-05-26 19:05:46 +0300

    Classes

--- lib/app.py
+++ lib/app.py
@@ -1,29 +1,26 @@
-##Functions --> These are reusable codes that execute when called
-# In python they are defined by the def keyword followed by the function name
+#Object oriented programming 
+class Item:
+    #Class
+    pay_rate = 0.8 #The pay rate after 20% discount
+    
+    def __init__(self,name = str, price = float ,quantity = 0):
+        #Run the validations to the received arguments
+        assert price >= 0 
+        assert quantity >= 0
+        
+        #Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+    def calculate_total_price(self):
+        return self.price * self.quantity
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+        pass
 
-def fun():
-    print("Python functions")
-fun()
-
-
-state = True
-def check_user():
-    if state != True:
-      print("Access granted")
-    else:
-       print("Access denied")
-
-check_user()
-
-#Inside the functions you can pass in arguments
-
-def passing_arg(amArg):
-    print(f"An argument has been passed in {amArg}")
-passing_arg("Python")
-
-weather = "rainy"
-
-weather_display = True
-
-if weather == "rainy":
-   print("Carry and umbrella")
+#Instances
+item1 = Item("Phone",100,1)
+item2 = Item("Laptop",1000,3)
+item3 = Item("Cable",10,5)
+item4 = Item("Mouse",50,5)
+item5 = Item("Keyboard",75,5)
