8d8c64fc4d412a0611c0a416d3cf0f438984c26a
CharlesKibet <charlesbiegon973@gmail.com>
2024-05-27 10:56:19 +0300

    Objects in python

---
+++ lib/object.py
@@ -0,0 +1,23 @@
+#Objects programming start over
+
+#!Objected oriented programming --> Is a type of programming based on the concept of "objects" which can contain data in the form of field  called (attributes or properties)
+
+#!Class --> Is a bundle of data and functionality. 
+#This is an example of a class and the name of the object should start with capital
+class Person:
+   #This is the class attribute
+    species = "Homo sapiens"
+
+#!Initialize --> This is to create a copy of  a class using it __init__ magical method or the dunder method
+    def __init__(self) -> None:
+      pass
+  
+  #Method --> This a function that is defined inside of a class
+    def profession(self):
+      pass
+  
+#!Object --The more common word used in the instance
+#This is now an instance
+person1 = Person()
+#This are the instance attributes
+person1 = "Charles"
