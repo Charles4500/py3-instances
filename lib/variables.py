d67073a553a6ba9f80944ff86bdc25321721db7f
CharlesKibet <charlesbiegon973@gmail.com>
2024-06-20 13:43:42 +0300

    Save all this new changes

---
+++ lib/variables.py
@@ -0,0 +1,38 @@
+# print("Hello World")
+#Python work over
+name = "charles"
+# update_name = name.lower()
+print(name[0].upper())
+# print(update_name)
+for word in name:
+  # "z" in word
+  print(word)
+
+#Swap 
+# a = input("Enter value of a")
+
+# b = input("Enter the value of b")
+
+#What will be stored in a will now move to temp
+# temp = a
+# What is now in a  will be put in b
+# a = b
+# Now what is in temp will be stored  inside b
+# b = temp
+# print(a != b)
+# print("The value in a is" + a)
+# print("The value of b is" + b)
+
+# number = input("Enter a two digits number: ") 
+# print(int(number[0]) + int(number[1]))
+num_1 = 4
+num_2 = 2
+result = int(num_1/num_2)
+print(result)
+
+a,b,c = 5,4,True
+if a <= 1 and c : print(a + b)
+
+print(a >> 2)
+ 
+
